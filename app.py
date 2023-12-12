131d8f1369429c3dbc3dfb4e54d5d5b811e770a5
Olivia Golden <olivia.golden.w@gmail.com>
2023-12-12 15:21:44 -0600

    create a cupcake

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 """Flask app for Cupcakes"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from models import db, connect_db, Cupcake
 
 app = Flask(__name__)
@@ -12,17 +12,35 @@ connect_db(app)
 app.app_context().push()
 db.create_all()
 
-@app.route("/cupcakes", methods=["GET"])
+@app.route("/api/cupcakes", methods=["GET"])
 def get_all_cupcakes():
     """ returns json of all cupcakes """
 
     json_cupcakes = Cupcake.json_list_cupcakes()
     return json_cupcakes
 
-@app.route("/cupcakes/<int:cupcake_id>", methods=["GET"])
+@app.route("/api/cupcakes/<int:cupcake_id>", methods=["GET"])
 def get_cupcake(cupcake_id):
     """ returns json of requested cupcake """
 
     cupcake = Cupcake.query.get_or_404(cupcake_id)
     serialized_cupcake = Cupcake.serialize_cupcake(cupcake)
     return jsonify(serialized_cupcake)
+
+@app.route("/api/cupcakes", methods=["POST"])
+def create_cupcake():
+    """ creates a cupcake, returns cupcake json """
+
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image = request.json["image"]
+
+    cupcake = Cupcake(flavor=flavor, size=size, rating=rating, image=image)
+
+    db.session.add(cupcake)
+    db.session.commit()
+
+    serialized_cupcake = Cupcake.serialize_cupcake(cupcake)
+
+    return (jsonify(cupcake=serialized_cupcake), 201)
