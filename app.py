0472c010a061f485253416d552b2215d3a0a0b18
Olivia Golden <olivia.golden.w@gmail.com>
2023-12-12 14:50:02 -0600

    cupcake model

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 """Flask app for Cupcakes"""
+from flask import Flask
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+app.app_context().push()
+db.create_all()
